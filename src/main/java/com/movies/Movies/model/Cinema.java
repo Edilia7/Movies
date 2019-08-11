49c4467b9a20317cbfe26b3d90209879e45d3ebb
Edilia <ediliagonzalezmateus7@gmail.com>
2019-08-11 12:28:01 -0600

    Adicionando projeto

---
+++ src/main/java/com/movies/Movies/model/Cinema.java
@@ -0,0 +1,46 @@
+package com.movies.Movies.model;
+
+public class Cinema {
+    private long id;
+    private String name, address, city, province;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+}
